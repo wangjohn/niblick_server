4553bf5edca7805df0448edc8fd986d63b5b675d
wangjohn <john.jian.wang@gmail.com>
2015-07-03 16:44:45 -0700

    Creating a simple API for posting rounds.

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+  "encoding/json"
+
+  "github.com/zenazn/goji"
+)
+
+func main() {
+  goji.Post("/v0/rounds", PostRounds)
+  goji.Serve()
+}
+
+const (
+  validHolesPerRound = []int{9, 18}
+)
+
+func PostRounds(w http.ResponseWriter, r *http.Request) {
+  var postRoundRequest PostRoundRequest
+  decoder := json.NewDecoder(r.Body)
+  decodeErr := decoder.Decode(&PostRoundRequest)
+  if (decodeErr != nil) {
+    // TODO(wangjohn): Write a wrapper that returns better validation errors
+    http.Error(w, decodeErr.Error(), http.StatusBadRequest)
+    return
+  }
+
+  roundLength := len(postRoundRequest.Holes)
+  if (roundLength != 9 && roundLength != 18) {
+    invalidRoundLengthMsg := fmt.Printf(
+      "Invalid number of holes in a round. Must have either 9 or 18 holes, not %v",
+      roundLength)
+    http.Error(w, invalidRoundLengthMsg, http.StatusBadRequest)
+    return
+  }
+
+  responseInterface := PostRoundResponse{"1"}
+  responseData, encodeErr := json.Marshal(responseInterface)
+  if (encodeErr != nil) {
+    http.Error(w, encodeErr.Error(), http.StatusInternalServerError)
+    return
+  }
+
+  w.Write(responseData)
+}
+
+type PostRoundResponse struct {
+  ID string
+}
+
+type PostRoundRequest struct {
+  Holes []Hole
+}
+
+type Hole struct {
+  Score int
+  Par int
+  Yardage int
+  Handicap int
+  GreenInReg bool
+  Fairway bool
+  Putts int
+}
